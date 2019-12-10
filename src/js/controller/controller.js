f5599a215f20ece840bc44ad59510502345b515a
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-10 18:11:34 -0300

    feat: builder and applying encapsulation

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -1,3 +1,25 @@
 class CalcController {
-    
+    constructor() {
+        this._displayCalc = '0'
+        this._actualDate;
+    }
+
+    /**
+     * Métodos GETTER AND SETTER
+     */
+    get displayCalc() {
+        return this._displayCalc
+    }
+
+    set displayCalc(value) {
+        this._displayCalc = value 
+    }
+
+    get actualDate() {
+        return this._actualDate
+    }
+
+    set actualDate(date) {
+        this._actualDate = date;
+    }
 }
