35c1267e1e084d3d3dad715b7c96d612d3224f0c
Daniel Silva <dani3l.dxp@gmail.com>
2020-01-21 13:20:57 -0300

    feat: formatting date and time to show on the calculator display

--- src/js/controller/controller.js
+++ src/js/controller/controller.js
@@ -1,25 +1,67 @@
 class CalcController {
-    constructor() {
-        this._displayCalc = '0'
-        this._actualDate;
-    }
+	constructor() {
+		this._locale = 'pt-BR';
+		this._display = document.querySelector('#display');
+		this._date = document.querySelector('#date');
+		this._time = document.querySelector('#time');
+		this._currentDate;
+		this.initialize();
+	}
 
-    /**
-     * Métodos GETTER AND SETTER
+	/**
+     * Other Methods
      */
-    get displayCalc() {
-        return this._displayCalc
-    }
+	initialize() {
+		this.setDisplayTime();
+		this.setDisplayDate();
+		setInterval(() => {
+			this.setDisplayTime();
+		}, 1000);
+	}
 
-    set displayCalc(value) {
-        this._displayCalc = value 
-    }
+	setDisplayTime() {
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+	}
 
-    get actualDate() {
-        return this._actualDate
-    }
+	setDisplayDate() {
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+			day: '2-digit',
+			month: 'short',
+			year: 'numeric'
+		});
+	}
+	/**
+     * Methods GETTER AND SETTER
+     */
+	get displayDate() {
+		return this._date.innerHTML;
+	}
 
-    set actualDate(date) {
-        this._actualDate = date;
-    }
+	set displayDate(value) {
+		this._date.innerHTML = value;
+	}
+
+	get displayTime() {
+		return this._time.innerHTML;
+	}
+
+	set displayTime(value) {
+		this._time.innerHTML = value;
+	}
+
+	get display() {
+		return this._display.innerHTML;
+	}
+
+	set display(value) {
+		this._display.innerHTML = value;
+	}
+
+	get currentDate() {
+		return new Date();
+	}
+
+	set currentDate(date) {
+		this._currentDate = date;
+	}
 }
